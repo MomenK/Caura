3a808193e46361f577ed86685b9fc5c0ee4c799e
MomenK <themonx11@gmail.com>
2018-03-07 12:07:27 +0000

    logo added

--- Views/Main.js
+++ Views/Main.js
@@ -621,7 +621,7 @@ style={{
 
 
 
-  containerComponent={<VictoryVoronoiContainer/>}
+  containerComponent={<VictoryVoronoiContainer voronoiDimension="x"/>}
  >
   <VictoryAxis domain={[0, 24]} tickValues={[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]}
   tickFormat={
